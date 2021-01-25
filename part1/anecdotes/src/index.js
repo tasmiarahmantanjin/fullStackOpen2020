4c24f3f2ee12cbc10049715edcb5fec6038ee117
Tasmia Rahman <tasmiata@gmail.com>
2021-01-25 17:25:03 +0200

    anecdotes app final version

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,62 +1,60 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-// Component Anacdote
-const Anecdote = ({ title, anecdote, votes }) => (
-  <div>
-    <h1>{title}</h1>
-    <div>{anecdote}</div>
-    <div>Has {votes} votes</div>
-  </div>
-);
+// import components from components directory
+import Button from "./components/Button"
+import Anecdote from "./components/Anecdote"
 
-const App = (props) => {
-// states
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState({
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-  })
+const App = ({ anecdotes }) => {
+	const [selected, setSelected] = useState(0);
 
-// generate random anecdote id
-const getRandomIndex = () => Math.floor(Math.random() * anecdotes.length);
+	// states for store the votes, in an array according to the anecdotes length filled with 0
+	const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
-// function to handle click for the vote button 
-const handleClick = () => {(
-  let id = getRandomIndex();
-  while (id === selected)
-    id = getRandomIndex
-)setSelected(selected + 1)}
+	// handleClickVote function to store the casted votes to an anecdote 
+	const handleClickVote = () => {
+		const newVotes = [...votes];
+		newVotes[selected]++;
+		setVotes(newVotes);
+	};
 
+	// handleClickNext function to find a random anecdotes using JS Math.floor & Math.random method
+	const handleClickNext = () => setSelected(Math.floor(Math.random() * anecdotes.length));
 
-  return (
-    <div>
+	// Function that returns the index of the anecdote with the most number of votes
+	const highestVote = () => votes.indexOf(Math.max(...votes));
 
-      {props.anecdotes[selected]}
-      <p>has {selected} vote</p>
-      <p>
-        <button onClick={(handleClick)}>vote </button>
-        <button onClick={(handleClick)}>next anecdote </button>
+	return (
+		<div>
+			<Anecdote
+				title={"Anecdote of the day"}
+				anecdote={anecdotes[selected]}
+				voteCount={votes[selected]}
+			/>
+	
+			<Button onClick={handleClickVote} text="vote" />
+			<Button onClick={handleClickNext} text="next anecdote" />
 
-      </p>
-    </div>
-  )
-}
+			<Anecdote
+				title={"Anecdote with most votes"}
+				anecdote={anecdotes[highestVote()]}
+				voteCount={votes[highestVote()]}
+			/>
+
+		</div>
+	);
+};
 
 const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+	'If it hurts, do it more often',
+	'Adding manpower to a late software project makes it later!',
+	'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+	'Premature optimization is the root of all evil.',
+	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
 
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
-  document.getElementById('root')
-)
+	<App anecdotes={anecdotes} />,
+	document.getElementById('root')
+);
